624f3d9c8ee8679fe5e57f604364d573828c1ccb
Yiran Zhu <yrzhu9@gmail.com>
2023-11-21 23:53:14 -0500

    Set up locust

---
+++ ramp.py
@@ -0,0 +1,55 @@
+from locust import HttpUser, TaskSet, task, constant
+from locust import LoadTestShape
+
+
+class UserTasks(TaskSet):
+    @task
+    def get_root(self):
+        self.client.get("/")
+
+
+class WebsiteUser(HttpUser):
+    tasks = [UserTasks]
+
+
+class StagesShape(LoadTestShape):
+    """
+    A simply load test shape class that has different user and spawn_rate at
+    different stages.
+
+    Keyword arguments:
+
+        stages -- A list of dicts, each representing a stage with the following keys:
+            duration -- When this many seconds pass the test is advanced to the next stage
+            users -- Total user count
+            spawn_rate -- Number of users to start/stop per second
+            stop -- A boolean that can stop that test at a specific stage
+
+        stop_at_end -- Can be set to stop once all stages have run.
+    """
+
+    stages = [
+        {"duration": 60, "users": 10, "spawn_rate": 2},
+        {"duration": 120, "users": 50, "spawn_rate": 5},
+        {"duration": 180, "users": 100, "spawn_rate": 10},
+        {"duration": 240, "users": 200, "spawn_rate": 15},
+        {"duration": 300, "users": 300, "spawn_rate": 20},
+        {"duration": 360, "users": 400, "spawn_rate": 25},
+        {"duration": 420, "users": 500, "spawn_rate": 30},
+        {"duration": 480, "users": 600, "spawn_rate": 35},
+        {"duration": 540, "users": 700, "spawn_rate": 40},
+        {"duration": 600, "users": 800, "spawn_rate": 45},
+        {"duration": 660, "users": 900, "spawn_rate": 50},
+        {"duration": 720, "users": 1000, "spawn_rate": 55},
+        {"duration": 780, "users": 1000, "spawn_rate": 60},
+    ]
+
+    def tick(self):
+        run_time = self.get_run_time()
+
+        for stage in self.stages:
+            if run_time < stage["duration"]:
+                tick_data = (stage["users"], stage["spawn_rate"])
+                return tick_data
+
+        return None
